f8c9b5146dd2acc95b08527321daed0f2653a56e
unknown <sthakur@IM-RT-IT-161.innomindshyd.com>
2015-12-11 18:54:16 +0530

    table design

---
+++ session/index.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html>
+
+  <body>
+
+    <table border="1">
+      <tr>
+        <td bgcolor="blue">
+   	      <img src="images.jpg" style="width:200px;height:100px;">
+        </td>
+        <td colspan="2" bgcolor="blue"><div id="main header">
+   	      <center><font color="white"><h1>GOWTHAM MODEL SCHOOL</h1></font></center>
+        </td>
+   
+      </tr>
+      <tr>
+  	   <td rowspan="2" bgcolor="orange">
+        <h1>about us
+        </h1>
+          Gowtham Model Schools in<br>
+          collaboration with K12 Techno Services<br>
+          is providing students with a <br>leading edge in the journey of life…<br>
+          so that they acquire the wings of knowledge<br> 
+          to soar higher into the infinite skies of opportunities on their<br> 
+          own as they prepare to leave our portal.<br> 
+
+        <h1>Progress</h1>
+          Gowtham has gained an excellent reputation<br>
+          through the outstanding achievements of our students.
+  
+        <iframe width="380" height="315" src="https://www.youtube.com/embed/s0oYxVifSGY" frameborder="0" allowfullscreen></iframe>
+
+	     </td>
+  	   <td width="500" bgcolor="orange" > 
+        <h3><center> OUR VISION</center></h3><br>
+        <center><img src="our vision.gif" style="width:300px;height:100px;">
+  	    </center>
+        <h3> <h3><center> student registration</center></h3><br>
+          <form autocomplete="on">
+            <center><p>First name:  <input type="text" name="first_name" placeholder="first name"  autofocus required/> </p>
+                    <p>Last name:  <input type="text" name="last_name" placeholder="last name"/></p>
+                    <p>User name :  <input type="text" name="user_id"  required placeholder="username@gmail.com" autocomplete="off"/></p>
+                    <p>confirm User name :  <input type="text" name="user_id"  required placeholder="username@gmail.com" autocomplete="off"/></p>
+                    <p>Password:<input type="password" name="password" required placeholder="************"/></p>
+                    <p>Gender:Male <input type="radio" name="Gender" value="Male" />
+                              Female <input type="radio" name="Gender" value="Female" /></p>
+                    <p>current class:<input type="number"  min="1" max="10" step="1"></p>
+                    <p>Date of Birth:<select name="Birth day" id="Birth Day">
+                        <option value="-1">Day:</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                        <option value="10">10</option>
+                        <option value="11">11</option>
+                        <option value="12">12</option>
+                        <option value="13">13</option>
+                        <option value="14">14</option>
+                        <option value="15">15</option>
+                        <option value="16">16</option>
+                        <option value="17">17</option>
+                        <option value="18">18</option>
+                        <option value="19">19</option>
+                        <option value="20">20</option>
+                        <option value="21">21</option>
+                        <option value="22">22</option>
+                        <option value="23">23</option>
+                        <option value="24">24</option>
+                        <option value="25">25</option>
+                        <option value="26">26</option>
+                        <option value="27">27</option>
+                        <option value="28">28</option>
+                        <option value="29">29</option>
+                        <option value="30">30</option>
+                        <option value="31">31</option>
+                        </select>
+                        <select id="Birthday_Month" name="Birthday_Month">
+                        <option value="-1">Month:</option>
+                        <option value="January">Jan</option>
+                        <option value="February">Feb</option>
+                        <option value="March">Mar</option>
+                        <option value="April">Apr</option>
+                        <option value="May">May</option>
+                        <option value="June">Jun</option>
+                        <option value="July">Jul</option>
+                        <option value="August">Aug</option>
+                        <option value="September">Sep</option>
+                        <option value="October">Oct</option>
+                        <option value="November">Nov</option>
+                        <option value="December">Dec</option>
+                        </select>
+                        <select name="Birthday_Year" id="Birthday_Year">
+                        <option value="-1">Year:</option>
+                        <option value="2012">2012</option>
+                        <option value="2011">2011</option>
+                        <option value="2010">2010</option>
+                        <option value="2009">2009</option>
+                        <option value="2008">2008</option>
+                        <option value="2007">2007</option>
+                        <option value="2006">2006</option>
+                        <option value="2005">2005</option>
+                        <option value="2004">2004</option>
+                        <option value="2003">2003</option>
+                        <option value="2002">2002</option>
+                        <option value="2001">2001</option>
+                        <option value="2000">2000</option>
+                        <option value="1999">1999</option>
+                        <option value="1998">1998</option>
+                        <option value="1997">1997</option>
+                        <option value="1996">1996</option>
+                        <option value="1995">1995</option>
+                        <option value="1994">1994</option>
+                        <option value="1993">1993</option>
+                        <option value="1992">1992</option>
+                        <option value="1991">1991</option>
+                        <option value="1990">1990</option>
+                        <option value="1989">1989</option>
+                        <option value="1988">1988</option>
+                        <option value="1987">1987</option>
+                        <option value="1986">1986</option>
+                        <option value="1985">1985</option>
+                        <option value="1984">1984</option>
+                        <option value="1983">1983</option>
+                        <option value="1982">1982</option>
+                        <option value="1981">1981</option>
+                        <option value="1980">1980</option>
+                        </select>
+                    </p>
+                    <p>Mobile number:<input type="text" name="Mobile_Number" maxlength="10" placeholder="10 digits" max="10" required/></p>
+                      Address:<textarea name="Address" rows="4" cols="30"></textarea></p>
+                    <p>City:<input type="text" name="City" maxlength="30" pattern="[A-Za-z]{20}" placeholder="max 20 characters a-z and A-Z"/></p>
+                    <p>Pincode:<input type="text" name="Pin_Code" maxlength="6" placeholder=" 6 digit number"/></p>
+                    <p>State<input type="text" name="State" maxlength="30" /></p>
+                    <p><button type="submit" value="Register">Submit</button>
+                       <button type="reset" value="Reset">Reset</button></p>
+            </center>
+          </form>
+      </td>
+      <td rowspan= "2" bgcolor="orange">
+        <div style="overflow:scroll;overflow-y:scroll;overflow-x:hidden">
+  		    <img src="annual.jpg" style="width:230px;height:150px;">
+  		      <br>
+  		      <h3>annual day celebration</h3><br>
+  		    <img src="cricket.jpg" style="width:230px;height:150px;"><br>
+  	 		    <h3>cricket champions</h3><br>
+  	 		  <img src="childrens.jpg" style="width:230px;height:150px;"><br>
+  	 		    <h3>childrens day celebration</h3>
+        </div>
+  	   </td>
+    </tr>
+    <tr>  
+  	  <td bgcolor="orange"><table>
+      <td bgcolor="yellowyellowyellow"> 
+  		  <img src="himayat nagar.jpg" style="width:230px;height:150px;">
+  	  </td>
+      <td bgcolor="yellowyellow">
+  	   	<img src="mehdipatnam.jpg" style="width:230px;height:150px;">
+  	  </td>
+  	  <td bgcolor="orange">
+  	 		<img src="charminar.jpg" style="width:230px;height:150px;">
+  	  </td>
+  
+ 
+  	</table>
+  	 	 </td>
+      </tr>
+    <tr>
+  	   <td colspan="2" bgcolor="orange"><center>Copyright © 2011 Gowtham Model Schools. All Rights Reserved.</center>
+  	   </td>
+  	   <td bgcolor="orange"><a href="#main header">TOP</a>
+  	   </td>
+     </tr>
+  	</table>
+  </body>
+</html>
